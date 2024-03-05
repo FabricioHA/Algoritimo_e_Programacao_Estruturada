f6b93a9ef5f01a5b41a9c08b96cf673f7c4ef594
FabricioHA <57398120+FabricioHA@users.noreply.github.com>
2024-03-05 18:58:57 -0300

    Add files via upload

---
+++ Aula_02/Exemplo_compostas.c
@@ -0,0 +1,17 @@
+/*Variaveis compostas são variáveis que podem armazenar mais de um valor 
+dentro delas, ou melhor, taxar vários espaços na memória com sua identificação.
+
+Com isso, elas são capazes de, por exemplo, em vez de reservar um bloco de 4bytes para,
+uma variável int, reservar vários blocos de 4bytes com seu mesmo nome por intermédio
+de indices, como se eu separasse varias gavetas (blocos) de um armário (memório) 
+apenas para guardar cuecas.
+
+As variáveis compostas podem lidar com um formato de linha e coluna, como se fossem
+matrizes.
+
+Temos dois tipos de variáveis compostas:
+
+Homogeneas e Unidimensionais
+
+
+*/
